6347bee9a1668b4bf2895cc7c5c647bc95236f9c
Tong <jeremy.tong@capgemini.com>
2018-09-18 15:19:09 -0400

    this is an example of a tightly coupled dependency

---
+++ src/main/java/com/spring/basics/springin5steps/BinarySearchImpl.java
@@ -0,0 +1,15 @@
+package com.spring.basics.springin5steps;
+
+public class BinarySearchImpl {
+	
+	public int binarySearch(int[] numbers, int numberToSearchFor) {
+//		Implementing Sorting Logic
+//		Bubble Sort Algorithm
+//		Quick Sort Algorithm
+//		Search the array
+		return 3;
+	}
+//	Sorting an array
+//	Search the array
+//	Return the result
+}
