37dff6b472fcc597edad99e8df03f21ff78801d0
Tong <jeremy.tong@capgemini.com>
2018-09-18 16:26:48 -0400

    Spring is now managing the dependencies we specified

--- src/main/java/com/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ src/main/java/com/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,6 +1,10 @@
 package com.spring.basics.springin5steps;
 
-public class BubbleSortAlgorithm implements SortAlgorithm{
+import org.springframework.stereotype.Component;
+
+@Component
+public class BubbleSortAlgorithm implements SortAlgorithm {
+	@Override
 	public int[] sort(int[] numbers) {
 //		Logic for Bubble Sort
 		return numbers;
--- src/main/java/com/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ src/main/java/com/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,16 +2,28 @@ package com.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
+//	What are the beans?
+//	What are the dependencies for the beans?
+//	Where to search for beans? = No Need
+
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-		int result = binarySearch.binarySearch(new int[] {12, 4, 6}, 3);
+//		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
+//		each new instance of an object is a bean ie new QuickSortAlgorithm() is a bean
+//		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
 //		com.spring.basics.springin5steps.BubbleSortAlgorithm@52e922
 //		com.spring.basics.springin5steps.QuickSortAlgorithm@52e922
+//		System.out.println(result);
+//		Appliction Context maintains all the beans
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
 		System.out.println(result);
-//		SpringApplication.run(SpringIn5StepsApplication.class, args);
+//		com.spring.basics.springin5steps.BubbleSortAlgorithm@1f8b5b8
+//		3
 	}
 }
