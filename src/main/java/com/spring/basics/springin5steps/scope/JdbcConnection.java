46f070edd1f5fd323f71885300a285fbe11c85d2
Tong <jeremy.tong@capgemini.com>
2018-09-19 13:40:29 -0400

    DAO same instance every time but jdbc instance is new every time

--- src/main/java/com/spring/basics/springin5steps/scope/JdbcConnection.java
+++ src/main/java/com/spring/basics/springin5steps/scope/JdbcConnection.java
@@ -1,8 +1,10 @@
 package com.spring.basics.springin5steps.scope;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE);
 public class JdbcConnection {
 	public JdbcConnection() {
 		System.out.println("JDBC Connection");
