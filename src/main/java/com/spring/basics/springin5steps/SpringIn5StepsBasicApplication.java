f33879f6febd6ff9ddbd15dbb392d9db4f884306
Tong <jeremy.tong@capgemini.com>
2018-09-19 16:46:02 -0400

    removed spring boot and got spring core working on the basic app

--- src/main/java/com/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/spring/basics/springin5steps/SpringIn5StepsBasicApplication.java
@@ -1,34 +1,29 @@
 package com.spring.basics.springin5steps;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.spring.basics.springin5steps.basic.BinarySearchImpl;
 
-@SpringBootApplication
+@Configuration
+@ComponentScan
 public class SpringIn5StepsBasicApplication {
 
-//	What are the beans?
-//	What are the dependencies for the beans?
-//	Where to search for beans? = No Need
-
 	public static void main(String[] args) {
-//		BinarySearchImpl binarySearch = new BinarySearchImpl(new QuickSortAlgorithm());
-//		each new instance of an object is a bean ie new QuickSortAlgorithm() is a bean
-//		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
-//		com.spring.basics.springin5steps.BubbleSortAlgorithm@52e922
-//		com.spring.basics.springin5steps.QuickSortAlgorithm@52e922
-//		System.out.println(result);
-//		Appliction Context maintains all the beans
-		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsBasicApplication.class, args);
+
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(
+				SpringIn5StepsBasicApplication.class);
+
 		BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+
 		BinarySearchImpl binarySearch1 = applicationContext.getBean(BinarySearchImpl.class);
+
 		System.out.println(binarySearch);
 		System.out.println(binarySearch1);
+
 		int result = binarySearch.binarySearch(new int[] { 12, 4, 6 }, 3);
 		System.out.println(result);
-//		com.spring.basics.springin5steps.BubbleSortAlgorithm@1f8b5b8
-//		3
 	}
 }
