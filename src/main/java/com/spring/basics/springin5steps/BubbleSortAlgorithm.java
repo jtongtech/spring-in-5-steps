59bbf7e0c2f109ff18100eadf78b55ef44ebc12b
Tong <jeremy.tong@capgemini.com>
2018-09-19 10:15:10 -0400

    fixing the intentional mistake we made to learn about the eroor logging

--- src/main/java/com/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ src/main/java/com/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,6 +1,8 @@
 package com.spring.basics.springin5steps;
 
-//@Component
+import org.springframework.stereotype.Component;
+
+@Component
 public class BubbleSortAlgorithm implements SortAlgorithm {
 	@Override
 	public int[] sort(int[] numbers) {
--- src/main/java/com/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.spring.basics.springin5steps;
 
-//
-//@Component
-//@Primary
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 	@Override
 	public int[] sort(int[] numbers) {
