206d084e4afdb4fc3a08f10c0eeac4a8bb24a5a8
Tong <jeremy.tong@capgemini.com>
2018-09-19 10:01:38 -0400

    removing the components from the class to see what it does in the logs

--- src/main/java/com/spring/basics/springin5steps/QuickSortAlgorithm.java
+++ src/main/java/com/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -1,8 +1,8 @@
 package com.spring.basics.springin5steps;
 
-import org.springframework.stereotype.Component;
-
-@Component
+//
+//@Component
+//@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 	@Override
 	public int[] sort(int[] numbers) {
