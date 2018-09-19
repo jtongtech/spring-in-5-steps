c311ec6d2603da79cebe580474c017372df7ca48
Tong <jeremy.tong@capgemini.com>
2018-09-19 11:38:33 -0400

    By adding prototype, you get a new instance each time the bean is called

--- src/main/java/com/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/spring/basics/springin5steps/BinarySearchImpl.java
@@ -2,9 +2,11 @@ package com.spring.basics.springin5steps;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class BinarySearchImpl {
 	@Autowired
 	@Qualifier("bubble")
