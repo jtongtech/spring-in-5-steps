dc287678c9066423ce5dfa9f9dbd9be7fa0b8c61
Tong <jeremy.tong@capgemini.com>
2018-09-19 11:14:53 -0400

    what happens if you have @Primary on 2 components?

--- src/main/java/com/spring/basics/springin5steps/BubbleSortAlgorithm.java
+++ src/main/java/com/spring/basics/springin5steps/BubbleSortAlgorithm.java
@@ -1,8 +1,10 @@
 package com.spring.basics.springin5steps;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class BubbleSortAlgorithm implements SortAlgorithm {
 	@Override
 	public int[] sort(int[] numbers) {
