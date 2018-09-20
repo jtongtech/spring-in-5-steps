cc250825f10fc09c7be86e8b18ca734730b912cd
Tong <jeremy.tong@capgemini.com>
2018-09-20 08:11:08 -0400

    Setting up files for XML

---
+++ src/main/java/com/spring/basics/springin5steps/xml/XMLPersonDAO.java
@@ -0,0 +1,14 @@
+package com.spring.basics.springin5steps.xml;
+
+public class XMLPersonDAO {
+	XMLJdbcConnection xmlJdbcConnection;
+
+	public XMLJdbcConnection getXmlJdbcConnection() {
+		return xmlJdbcConnection;
+	}
+
+	public void setXmlJdbcConnection(XMLJdbcConnection xmlJdbcConnection) {
+		this.xmlJdbcConnection = xmlJdbcConnection;
+	}
+
+}
